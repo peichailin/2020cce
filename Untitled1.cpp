7c76ab0ab3589cb8af08ad3d2b5a8748cccc1ee2
peichailin <79628640+peichailin@users.noreply.github.com>
2021-05-20 17:11:33 +0800

    Rename Untitled1.cpp to week13/Untitled1.cpp

--- Untitled1.cpp
+++
@@ -1,9 +0,0 @@
-void setup()//只做一次設定
-{
-  size(1024, 400);
-}
-void draw()//互動版本,每秒畫60次
-{
-  if(mousePressed) background(255,0,255);//mousePressed當滑鼠按下去時,紫色
-  else background(168,154,250);//否則就是這個顏色,淡紫色
-}
