d8f8cc56908f54caa800649808aaa8a5a75992b3
peichailin <79628640+peichailin@users.noreply.github.com>
2021-06-03 15:06:19 +0800

    Rename Untitled1.cpp to week15/Untitled1.cpp

--- week15/Untitled1.cpp
+++ week15/Untitled1.cpp
@@ -2,9 +2,9 @@ void setup()
 {
     size(400,200);
 }
-void draw()//¨C¬íµe60¦¸
+void draw()//Â¨CÂ¬Ã­Âµe60Â¦Â¸
 {
-    int s=second();//0~59¬í
+    int s=second();//0~59Â¬Ã­
     if(s%2==0) background(255,0,0);
     else background(58,114,191);
 }
